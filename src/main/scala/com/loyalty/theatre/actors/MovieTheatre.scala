272deb8bb27e110953ab4c75be637d45c60fb78d
cfernandes <cfernandes@loyalty.com>
2017-10-30 18:27:11 -0400

    flesh out Movie Theatre actor some more

--- src/main/scala/com/loyalty/theatre/actors/MovieTheatre.scala
+++ src/main/scala/com/loyalty/theatre/actors/MovieTheatre.scala
@@ -1,12 +1,19 @@
 package com.loyalty.theatre.actors
 
+import java.util.UUID
+
 import akka.actor.{Actor, ActorLogging}
 
-class MovieTheatre extends Actor with ActorLogging {
+class MovieTheatre(seatingCapacity: Int) extends Actor with ActorLogging {
   override def receive: Receive = ???
 }
 
 object MovieTheatre {
+  case class State(remainingSeats: Int)
+
   sealed trait Command
   final case class BookTicket(quantity: Int) extends Command
+
+  sealed trait Event
+  final case class TicketBooked(quantity: Int, receipt: UUID) extends Event
 }
